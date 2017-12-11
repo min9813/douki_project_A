f3633ac6d0ea1fba2cfce414cfbbdd34d218536d
futakw <fuwafuwa1118@gmail.com>
2017-12-11 17:43:44 +0900

    期限の追加
    期限の追加

--- to_do_list/to_do.js
+++ to_do_list/to_do.js
@@ -29,15 +29,23 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
+
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
+  var inputDateValue = document.getElementById("myInputDate").value;
+
   li.appendChild(t);
+
+
   if (inputValue === '') {
     alert("You must write something!");
-  } else {
+  } else if (inputDateValue === '') {
+    alert("You must write date!");
+  }else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
+  document.getElementById("myInputDate").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -45,10 +53,18 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
+  var span0 = document.createElement("SPAN0");
+  var txt0 = document.createTextNode(inputDateValue);
+  span0.className = "date";
+  span0.appendChild(txt0);
+  li.appendChild(span0);
+
+
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
+
 }
